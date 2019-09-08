35de661c28e92d7280b5c7b37b9588b77227163e
aaron <853126386@qq.com>
2019-09-09 00:42:57 +0800

    style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts/default')
 @section('content')
-    <h1>主页</h1>
+
+    <div class="jumbotron">
+        <h1>hello world</h1>
+        <p class="lead">
+            你所看到的是 <a href="">laravel入门教程</a>实例页
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a href="" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
     @stop
 
