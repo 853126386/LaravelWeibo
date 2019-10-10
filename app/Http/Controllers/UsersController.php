e5c20afe1664ff90e0b296453c0c10937ca83b07
github <lwt@163.com>
2019-10-10 14:11:51 +0800

    注å注册成功操作

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,8 +21,15 @@ class UsersController extends Controller
         $this->validate($request,[
             'name'=>'required|max:50',
             'email'=>'required|email|unique:users|max:255',
-            'password'=>'required|confirm|max:6',
+            'password'=>'required|confirmed|max:6',
         ]);
-        return  ;
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        return  redirect()->route('users.show',[$user]);
     }
 }
