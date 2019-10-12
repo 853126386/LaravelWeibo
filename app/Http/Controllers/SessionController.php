0ee8ca48c70a36d53517b84d712bba047c9980d0
github <lwt@163.com>
2019-10-12 12:03:17 +0800

    穿ä创建回è会话

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionController extends Controller
+{
+    //
+
+    public function create(){
+        return view('session.create');
+    }
+
+    public function store(Request $request){
+        $credentials=$this->validate($request,[
+           'email'=>'required|email|max:255',
+           'password'=>'required',
+        ]);
+        if (Auth::attempt($credentials)) {
+            session()->flash('success','欢迎回来');
+            return redirect()->route('users.show',[Auth::user()]);
+            // 登录成功后的相关操作
+        } else {
+            session()->flash('danger','很抱歉！你的邮箱或者密码错误');
+            return redirect()->back()->withInput();
+            // 登录失败后的相关操作
+        }
+
+        return ;
+
+    }
+
+}
+
