19c5bfd3be076e7f699638bed7b04a1c0a5edd44
github <lwt@163.com>
2019-10-14 16:19:03 +0800

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,4 +34,32 @@ class UsersController extends Controller
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return  redirect()->route('users.show',[$user]);
     }
+
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user){
+        return  view('users.edit',compact('user'));
+    }
+
+    /**
+     * @param User $user
+     * @param Request $request
+     */
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+           'name'=>'required|max:50',
+           'password'=>'required|confirmed|min:6',
+        ]);
+        $data=['name'=>$request->name];
+        if($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','编辑资料成功');
+        return redirect()->route('users.show',$user->id);
+    }
+
 }
